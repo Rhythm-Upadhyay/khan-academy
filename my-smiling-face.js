eec046974401e4080ed715bfd5383fad0425ee7b
Rhythm-Upadhyay <67551843+Rhythm-Upadhyay@users.noreply.github.com>
2020-06-28 21:20:10 +0530

    Create my-smiling-face.js

---
+++ my-smiling-face.js
@@ -0,0 +1,30 @@
+background(0, 255, 179);
+
+//face
+fill(252, 239, 0);
+ellipse(200, 200, 336, 346);                                                        
+
+//mouth
+fill(250, 20, 20);
+ellipse(201, 244, 227, 38);
+
+//eyes
+fill(245, 235, 235);
+ellipse(118, 122, 82, 76); //left 
+ellipse(274, 121, 83, 72); //right
+
+fill(168, 162, 224);
+triangle(205, 140, 265, 197, 142, 197); //nose
+
+//nose nastorl
+fill(54, 37, 37);
+ellipse(184, 179, 19, 15);//right
+ellipse(215, 177, 19, 13);//left
+
+//eyeballs
+ellipse(139, 109, 19, 13);//right
+ellipse(291, 109, 19, 13);//left
+
+//head band
+strokeWeight(20);
+line(109, 56 ,293 ,51 );
